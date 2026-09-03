0e5b7a0da5db7026016ab9646353320289ecb5b3
SaquibAnjum <saquib1312@gmail.com>
2026-09-03 22:38:01 +0530

    home

--- src/file_upload/__init__.py
+++ src/file_upload/__init__.py
@@ -11,7 +11,7 @@ app.config['JWT_SECRET_KEY'] = "sjekci348dfktocfmekxcjemtkxmejfj5k3o"
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(task_bp)
 
-app.get("/")
+@app.get("/")
 def home():
     return "Welcome to the Flask API!"
 
